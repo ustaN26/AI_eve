ad4c16f5c68a445a3961822be4aea36e97c79c1f
BuildTools <unconfigured@null.spigotmc.org>
2023-10-02 13:08:29 +0200

    manager commencé

--- Eve/src/fr/eve/main/Deplacement.java
+++ Eve/src/fr/eve/main/Deplacement.java
@@ -5,26 +5,27 @@ import lejos.hardware.port.MotorPort;
 import lejos.utility.Delay;
 
 public class Deplacement {
-	private static final EV3LargeRegulatedMotor mG= new EV3LargeRegulatedMotor(MotorPort.C);
-	private static final EV3LargeRegulatedMotor mD= new EV3LargeRegulatedMotor(MotorPort.B);
-	
+	private static final EV3LargeRegulatedMotor mG = new EV3LargeRegulatedMotor(MotorPort.C);
+	private static final EV3LargeRegulatedMotor mD = new EV3LargeRegulatedMotor(MotorPort.B);
+
 	public static void Avancer(int temps) {
-	mG.synchronizeWith(new EV3LargeRegulatedMotor[] {mD});
-	
-	mG.startSynchronization();
-	mG.forward();
-	mD.forward();
-	mG.endSynchronization();
-	
-	Delay.msDelay(temps);
-	
-	mG.startSynchronization();
-	mG.close();
-	mD.close();
-	mG.endSynchronization();
+		mG.synchronizeWith(new EV3LargeRegulatedMotor[] { mD });
+
+		mG.startSynchronization();
+		mG.forward();
+		mD.forward();
+		mG.endSynchronization();
+
+		Delay.msDelay(temps);
+
+		mG.startSynchronization();
+		mG.close();
+		mD.close();
+		mG.endSynchronization();
 	}
+
 	public static void Reculer(int temps) {
-		mG.synchronizeWith(new EV3LargeRegulatedMotor[] {mD});
+		mG.synchronizeWith(new EV3LargeRegulatedMotor[] { mD });
 		mG.startSynchronization();
 		mG.backward();
 		mD.backward();
@@ -34,22 +35,28 @@ public class Deplacement {
 		mG.close();
 		mD.close();
 		mG.endSynchronization();
-		}
+	}
+
 	public static void RotationDroite(int angle) {
-		mG.rotate(angle);;
+		mG.rotate(angle);
+		;
 	}
+
 	public static void RotationGauche(int angle) {
-		mD.rotate(angle);;
+		mD.rotate(angle);
+		;
 	}
+
 	public static void AvancerDiago() {
 		RotationDroite(45);
 		Avancer(5000);
 	}
+
 	public static void RotationRapide(int angle) {
-		mG.synchronizeWith(new EV3LargeRegulatedMotor[] {mD});
+		mG.synchronizeWith(new EV3LargeRegulatedMotor[] { mD });
 		mG.startSynchronization();
 		mG.rotate(angle);
-		mD.rotate(- angle);
+		mD.rotate(-angle);
 		mG.endSynchronization();
 		mG.waitComplete();
 		mD.waitComplete();
@@ -58,6 +65,6 @@ public class Deplacement {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		RotationRapide(800);
-	} 
+	}
 
 }
---
+++ Eve/src/fr/eve/main/TestPince.java
@@ -0,0 +1,25 @@
+package fr.eve.main;
+
+import lejos.hardware.BrickFinder;
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
+
+public class TestPince {
+
+	public static void main(String[] args) {
+		/* private static */
+		float MIN_ANGLE_PINCE = 0;
+		float MAX_ANGLE_PINCE = 0;
+		final EV3LargeRegulatedMotor motorPince = new EV3LargeRegulatedMotor(BrickFinder.getDefault().getPort("A"));
+		motorPince.setStallThreshold(1, 100);
+		motorPince.setSpeed(motorPince.getMaxSpeed());
+		while(motorPince.isMoving() || !motorPince.isStalled())motorPince.backward();
+		MIN_ANGLE_PINCE = motorPince.getPosition();
+		while(!motorPince.isStalled())motorPince.forward();
+		MAX_ANGLE_PINCE = motorPince.getPosition();
+		while(!motorPince.isStalled())motorPince.backward();
+		MIN_ANGLE_PINCE = motorPince.getPosition();
+
+		BrickFinder.getDefault().getGraphicsLCD().drawString(""+MIN_ANGLE_PINCE, 0, 0, 0);
+		BrickFinder.getDefault().getGraphicsLCD().drawString(""+MAX_ANGLE_PINCE, 0, 40, 0);
+	}
+}
