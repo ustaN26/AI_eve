0bd1e5e9464d81d01c88f2fb09671e810358b781
robin <robinhuber@laposte.net>
2023-12-04 19:38:21 +0100

    javadoc de sensors et Tester/sousclass

--- Eve/fr/eve/main/BAU.java
+++ Eve/fr/eve/main/BAU.java
@@ -5,10 +5,14 @@ import lejos.hardware.Button;
 import lejos.hardware.Key;
 import lejos.hardware.KeyListener;
 
+/**
+ * Class Bouton d'Arret d'Urgence.
+ * arrete le programme lorsque ESCAPE est appuye
+ */
 public class BAU {
 	public static void bau() {
 		BrickFinder.getDefault().getKey(Button.ESCAPE.getName()).addKeyListener(new KeyListener() {
-			@Override // boutton arret d'urgance
+			@Override // boutton arret d'urgence
 			public void keyReleased(Key k) {
 				System.exit(0);
 			}
--- Eve/fr/eve/main/test/TestPremierPalet.java
+++ Eve/fr/eve/main/test/TestPremierPalet.java
@@ -3,15 +3,9 @@ import fr.eve.main.Activators;
 import fr.eve.main.BAU;
 import fr.eve.main.Constantes;
 import fr.eve.main.Sensors;
-import fr.eve.main.Brain.Etats;
 import fr.eve.main.tester.Tester;
 import fr.eve.main.tester.TouchTest;
 import fr.eve.main.tester.USTest;
-import lejos.hardware.BrickFinder;
-import lejos.hardware.Button;
-import lejos.hardware.Key;
-import lejos.hardware.KeyListener;
-import lejos.utility.Delay;
 
 public class TestPremierPalet implements Constantes{
 	private Activators activators;
